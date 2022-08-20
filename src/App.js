2586942c0f2d89eb995da10031e13df7b943c255
mangeshipatil <thisismangeshi@gmail.com>
2022-08-20 23:12:00 +0530

    added basic countdown with start and stop button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import { useEffect, useRef, useState } from 'react';
+import {useRef, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [initialState, setInitialState] = useState(5)
+  const [initialState, setInitialState] = useState(500)
+  const [isPause, setPause] = useState(false)
   const countRef = useRef()
 
-  console.log(countRef.current)
+  const formatTime = () => {
+    const getSecond = `0${initialState % 60}`.slice(-2);
+    const minutes = `${Math.floor(initialState / 60)}`;
+    const getMinute = `0${minutes % 60}`.slice(-2);
 
-  const handlePlay=()=>{
+    return ` ${getMinute} : ${getSecond} `;
+  };
+
+  const handlePlay = () => {
+    setPause(true)
     clearInterval(countRef.current)
-      countRef.current = setInterval(() => {
-          setInitialState((cd) => cd - 1);
-      }, 1000);
+    countRef.current = setInterval(() => {
+      setInitialState((cd) => cd - 1);
+    }, 1000);
   }
 
+  const handlePause = () => {
+    clearInterval(countRef.current)
+    setPause(false)
+  }
   return (
     <div className="App">
       <button onClick={handlePlay}>Start</button>
-      <h1>{initialState}</h1>
+      <button onClick={handlePause}>Pause</button>
+
+      <h1>{formatTime()}</h1>
     </div>
   );
 }
