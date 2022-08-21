0863989b20a84b100ecb33a7286134e456142e2b
mangeshipatil <thisismangeshi@gmail.com>
2022-08-21 18:47:35 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const [timeLeft, settimeLeft] = useState(5*60)
   const [isPaused, setPause] = useState(false)
+
   const countRef = useRef()
 
 
@@ -37,9 +38,43 @@ function App() {
 
   return (
     <div className="App">
-      {!isPaused ? (<button onClick={handlePlay}>Start</button>) : (<button onClick={handlePause}>Pause</button>)}
+      {/* {!isPaused ? (<button onClick={handlePlay}>Start</button>) : (<button onClick={handlePause}>Pause</button>)}
       <button onClick={handleReset}>Reset</button>
-      <h1><FormatTime timeLeft={timeLeft}/></h1>
+      <h1><FormatTime timeLeft={timeLeft}/></h1> */}
+      <div className='containerr'>
+        <div className='wrapper'>
+
+        <div className='displayTimeLeftCont'>
+          <p className='behaviour'>Session</p>
+          <h1 className='time-left'>01:00</h1>
+        </div>
+        <div className='button-container'>
+          {!isPaused ? (<button className='start btnn'>Start</button>) :(<button className='stop btnn'>Stop</button>)}
+          <button className='reset btnn'>Reset</button>
+        </div>
+
+        <div className='inc-dec-btns '>
+          <div className='session-cont col-md-6'>
+            <p>Session Length</p>
+            <div className='session-btn'>
+              <button className='increment btnn'>+</button>
+              <div className='displaySessionValue'>25</div>
+              <button className='decrement btnn'>-</button>
+            </div>
+          </div>
+          <div className='break-cont col-md-6'>
+            <p>Break Length</p>
+            <div className='break-btn'>
+            <button className='increment btnn'>+</button>
+              <div className='displayBreakValue'>5</div>
+            <button className='decrement btnn'>-</button>
+
+            </div>
+          </div>
+        </div>
+
+        </div>
+      </div>
     </div>
   );
 }
