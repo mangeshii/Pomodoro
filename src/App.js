893ddaf314472a28e76eb0b58ef2be3d28dfebb1
mangeshipatil <thisismangeshi@gmail.com>
2022-08-21 22:02:15 +0530

    added pomodoro clock

--- src/App.js
+++ src/App.js
@@ -4,25 +4,73 @@ import FormatTime from './FormatTime';
 
 function App() {
 
-  const [timeLeft, settimeLeft] = useState(5*60)
-  const [isPaused, setPause] = useState(false)
+  const [isPaused, setPause] = useState(false);
+  const [session, setSession] = useState(25);
+  const [breakk, setBreak] = useState(5);
+  const [timeLeft, settimeLeft] = useState(25 * 60);
+  console.log(timeLeft)
+  const [behaviour, setBehaviour] = useState('Session')
 
   const countRef = useRef()
 
+  const switchesTimerMode = () => {
+    if (behaviour == 'Session') {
+      setBehaviour('Break')
+      settimeLeft(breakk*60)
+    }
+    else {
+      setBehaviour('Session')
+      settimeLeft(session)
+    }
+  }
 
   const handlePlay = () => {
     setPause(true)
+
       countRef.current = setInterval(() => {
         settimeLeft((cd) => cd - 1);
       }, 1000);
 
+  
   }
 
   useEffect(()=>{
-    if(timeLeft===0){
-      clearInterval(countRef.current)
+    if(timeLeft==0){
+      switchesTimerMode()
     }
-  },[timeLeft])
+  })
+
+  const sessionIncrement = () => {
+    if (session <= 59) {
+      setSession(session + 1)
+      settimeLeft((session + 1) * 60)
+
+    }
+  }
+
+  const sessionDecrement = () => {
+    if (session > 1) {
+      setSession(session - 1)
+      settimeLeft((session - 1) * 60)
+    }
+  }
+
+
+  const breakIncrement = () => {
+    if (breakk <= 59) {
+      setBreak(breakk + 1)
+    }
+  }
+
+
+  const breakDecrement = () => {
+    if (breakk > 1) {
+      setBreak(breakk - 1)
+    }
+  }
+
+
+ 
 
 
   const handlePause = () => {
@@ -30,48 +78,47 @@ function App() {
     setPause(false)
   }
 
-  const handleReset=()=>{
-    clearInterval(countRef.current)
-    settimeLeft(5*60)
-  }
+  const FormatTime = () => {
+    const getSecond = `0${timeLeft % 60}`.slice(-2);
+    const minutes = `${Math.floor(timeLeft / 60)}`;
+    const getMinute = `0${minutes % 60}`.slice(-2);
 
+    return ` ${getMinute} : ${getSecond} `;
+  }
 
   return (
     <div className="App">
-      {/* {!isPaused ? (<button onClick={handlePlay}>Start</button>) : (<button onClick={handlePause}>Pause</button>)}
-      <button onClick={handleReset}>Reset</button>
-      <h1><FormatTime timeLeft={timeLeft}/></h1> */}
       <div className='containerr'>
         <div className='wrapper'>
 
-        <div className='displayTimeLeftCont'>
-          <p className='behaviour'>Session</p>
-          <h1 className='time-left'>01:00</h1>
-        </div>
-        <div className='button-container'>
-          {!isPaused ? (<button className='start btnn'>Start</button>) :(<button className='stop btnn'>Stop</button>)}
-          <button className='reset btnn'>Reset</button>
-        </div>
+          <div className='displayTimeLeftCont'>
+            <p className='behaviour'>{behaviour}</p>
+            <h1 className='time-left'>{FormatTime()}</h1>
+          </div>
+          <div className='button-container'>
+            {!isPaused ? (<button className="start btnn" onClick={handlePlay}>Start</button>) : (<button className="stop btnn" onClick={handlePause}>Pause</button>)}
+            <button className='reset btnn'>Reset</button>
+          </div>
 
-        <div className='inc-dec-btns '>
-          <div className='session-cont col-md-6'>
-            <p>Session Length</p>
-            <div className='session-btn'>
-              <button className='increment btnn'>+</button>
-              <div className='displaySessionValue'>25</div>
-              <button className='decrement btnn'>-</button>
+          <div className='inc-dec-btns '>
+            <div className='session-cont col-md-6'>
+              <p>Session Length</p>
+              <div className='session-btn'>
+                <button className='increment btnn' onClick={sessionIncrement}>+</button>
+                <div className='displaySessionValue'>{session}</div>
+                <button className='decrement btnn' onClick={sessionDecrement}>-</button>
+              </div>
+            </div>
+            <div className='break-cont col-md-6'>
+              <p>Break Length</p>
+              <div className='break-btn'>
+                <button className='increment btnn' onClick={breakIncrement}>+</button>
+                <div className='displayBreakValue'>{breakk}</div>
+                <button className='decrement btnn' onClick={breakDecrement}>-</button>
+
+              </div>
             </div>
           </div>
-          <div className='break-cont col-md-6'>
-            <p>Break Length</p>
-            <div className='break-btn'>
-            <button className='increment btnn'>+</button>
-              <div className='displayBreakValue'>5</div>
-            <button className='decrement btnn'>-</button>
-
-            </div>
-          </div>
-        </div>
 
         </div>
       </div>
@@ -82,3 +129,37 @@ function App() {
 export default App;
 
 
+
+{/* {!isPaused ? (<button onClick={handlePlay}>Start</button>) : (<button onClick={handlePause}>Pause</button>)}
+<button onClick={handleReset}>Reset</button>
+// const [timeLeft, settimeLeft] = useState(5*60)
+// const [isPaused, setPause] = useState(false)
+
+// const countRef = useRef()
+
+
+// const handlePlay = () => {
+//   setPause(true)
+//     countRef.current = setInterval(() => {
+//       settimeLeft((cd) => cd - 1);
+//     }, 1000);
+
+// }
+
+// useEffect(()=>{
+//   if(timeLeft===0){
+//     clearInterval(countRef.current)
+//   }
+// },[timeLeft])
+
+
+// const handlePause = () => {
+//   clearInterval(countRef.current)
+//   setPause(false)
+// }
+
+// const handleReset=()=>{
+//   clearInterval(countRef.current)
+//   settimeLeft(5*60)
+// }
+<h1><FormatTime timeLeft={timeLeft}/></h1> */}
